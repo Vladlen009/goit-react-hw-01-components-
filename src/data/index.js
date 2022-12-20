9206580dff7e7ca5c999dd22c604705826d7678e
Vladyslav Belotskiy <vlad.belotskiy9595@gmail.com>
2022-12-20 22:17:34 +0200

    2

---
+++ src/data/index.js
@@ -0,0 +1,6 @@
+import data from './data.json';
+import user from './user.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
+
+export { data, friends, user, transactions };
